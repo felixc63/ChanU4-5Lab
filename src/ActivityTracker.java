5444a56843d493c76769a0984032a0ae21004487
BT_3W2_22 <annac101@nycstudents.net>
2022-12-09 12:11:22 -0500

    Finish

--- src/ActivityTracker.java
+++ src/ActivityTracker.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class ActivityTracker {
+public class ActivityTracker {
+    private int targetGoal;
+    private int totalMinutes;
+    private int totalLogs;
+    private int maxMinutes;
+
+    public ActivityTracker(int targetGoal){
+        this.targetGoal = targetGoal;
+    }
+
+    public void logMinutes(int amount){
+        totalMinutes += amount;
+        if (amount > maxMinutes){
+            maxMinutes = amount;
+        }
+        totalLogs++;
+    }
+
+    public int getSessions(){
+        return totalLogs;
+    }
+
+    public int getTotalMinutes(){
+        return totalMinutes;
+    }
+
+    public int getMaxMinutes(){
+        return maxMinutes;
+    }
+
+    public boolean goalAchieved(){
+        if (totalMinutes >= targetGoal){
+            return true;
+        }
+        return false;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your goal in minutes: ");
+        int goal = scan.nextInt();
+
+        ActivityTracker activityTracker = new ActivityTracker(goal);
+
+        int count = 0;
+        while(true){
+            System.out.print("Enter the number of minutes for your activity session, - 1 to end: ");
+            count = scan.nextInt();
+            if (count == -1){
+                break;
+            }
+            activityTracker.logMinutes(count);
+        }
+        System.out.println();
+
+        System.out.println("Good job being active! Let's look at your stats:");
+        System.out.println("Number of sessions: " + activityTracker.getSessions());
+        System.out.println("Total number of minutes: " + activityTracker.getTotalMinutes());
+        System.out.println("Longest session logged (max minutes): " + activityTracker.getMaxMinutes());
+        System.out.println("You reached your goal: " + activityTracker.goalAchieved());
+
+    }
 }
